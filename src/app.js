ac067626f25d3f2ef9a11846d2d6c667c0871aa4
Tauane Costa <tauanedasilvacosta@gmail.com>
2020-12-20 13:04:50 -0300

    Node Challenge Solution

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -9,25 +8,102 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+const likesRepo = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  if (repositories < 0) {
+    return response.json({ error: 'data not found' });
+  }
+
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+  const id = uuid();
+
+  if (likesRepo.findIndex(element => element.id === id) == -1) {
+    let numberOfLikes = 0;
+
+    const newLikeObject = {
+      id,
+      numberOfLikes
+    }
+
+    likesRepo.push(newLikeObject);
+  }
+
+  const indexOfLikes = likesRepo.findIndex(element => element.id === id);
+  const likes = likesRepo[indexOfLikes].numberOfLikes;
+
+
+  const newInfo = {
+    id,
+    url,
+    title,
+    techs,
+    likes
+  }
+
+  repositories.push(newInfo);
+
+  return response.json(newInfo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  if (repositories.findIndex(repo => repo.id === id) == -1) {
+    return response.status(400).json({ error: 'Bad Request' });
+  }
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+  const repoTest = repositories[repositorieIndex];
+
+  repoTest.url = url;  
+  repoTest.title = title;
+  repoTest.techs = techs;
+
+  repositories[repositorieIndex] = repoTest;
+
+  return response.json(repositories[repositorieIndex]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (repositories.findIndex(repo => repo.id === id) == -1) {
+    return response.status(400).json({ error: 'Bad Request' });
+  }
+
+  const repoIndex = repositories.findIndex(element => element.id === id);
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (repositories.findIndex(repo => repo.id === id) == -1) {
+    return response.status(400).json({ error: 'Bad Request' });
+  }
+
+  const index = likesRepo.findIndex(element => element.id === id);
+  const specifLike = likesRepo[index];
+  specifLike.numberOfLikes++;
+
+  const indexRepo = repositories.findIndex(element => element.id === id);
+  const specifLikeRepo = repositories[indexRepo];
+  specifLikeRepo.likes = specifLike.numberOfLikes;
+
+  return response.json({"likes": specifLike.numberOfLikes});
 });
 
 module.exports = app;
